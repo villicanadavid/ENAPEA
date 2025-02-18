8cd6992424cf5e7b6d52a9a3e8ed130db005454a
David Villicana <villicanad0807@gmail.com>
2025-02-17 23:09:25 -0600

    Update DEFUN_ELT.py
    FUNCIONAL

--- DEFUN_ELT.py
+++ DEFUN_ELT.py
@@ -11,58 +11,32 @@ spark = SparkSession.builder.appName("DEFUN_ELT").getOrCreate()
 dbf_folder = "/content/drive/MyDrive/DEFUN/"
 parquet_folder = "/content/drive/MyDrive/DEFUN/parquet_files/"
 os.makedirs(parquet_folder, exist_ok=True)
-spark = SparkSession.builder.appName("DBFtoParquet").getOrCreate()
 dbf_files = [f for f in os.listdir(dbf_folder) if f.endswith(".dbf")]
-
 for dbf_file in dbf_files:
     dbf_path = os.path.join(dbf_folder, dbf_file)
     parquet_path = os.path.join(parquet_folder, dbf_file.replace(".dbf", ".parquet"))
     try:
         dbf = Dbf5(dbf_path, codec="utf-8")
         df_pandas = dbf.to_dataframe()
+        
+        for col in df_pandas.columns:
+            df_pandas[col] = df_pandas[col].astype(str)
         df_pandas.to_parquet(parquet_path, engine="pyarrow")
         print(f"✅ Convertido: {dbf_file} → {parquet_path}")
     except Exception as e:
         print(f"❌ Error al procesar {dbf_file}: {e}")
-      
-#------------------------------------------------------------------Convertir .dbf a parquet para optimizar procesamiento
+
+#------------------------------------------------------------------Obtener DF EDR
 
 parquet_files = [os.path.join(parquet_folder, f) for f in os.listdir(parquet_folder) if f.endswith(".parquet")]
-DEFUN = spark.read.parquet(*parquet_files)
-DEFUN_GRAL = DEFUN.filter(
-    (col("Anio_ocur").between(2005, 2030)) & 
-    (col("Embarazo").isin(1, 2, 3, 4))
-)
-
-DEFUN_ENAPEA = DEFUN_GRAL.filter(col("Edad") <= 19)
-
-DEFUN_GRAL = DEFUN_GRAL.select(
-    col("Anio_ocur").cast(IntegerType()),
-    col("Ent_resid").alias("Ent_ID").cast(IntegerType()),
-    col("Mun_resid").alias("Mun_ID").cast(IntegerType()),
-    col("Loc_resid").alias("Loc_ID").cast(IntegerType()),
-    col("Edad").cast(IntegerType()),
-    col("Causa_def").cast(StringType()),
-    col("Embarazo").cast(BooleanType()),
-    col("Rel_emba").cast(BooleanType())
-)
-
-DEFUN_ENAPEA = DEFUN_ENAPEA.select(
-    col("Anio_ocur").cast(IntegerType()),
-    col("Ent_resid").alias("Ent_resid_ID").cast(IntegerType()),
-    col("Mun_resid").alias("Mun_resid_ID").cast(IntegerType()),
-    col("Loc_resid").alias("Loc_resid_ID").cast(IntegerType()),
-    col("Edad").cast(IntegerType()),
-    col("Causa_def").cast(StringType()),
-    col("Edo_civil").cast(StringType()),
-    col("Escolarida").cast(StringType()),
-    col("Sitio_ocur").cast(StringType()),
-    col("Derechohab").cast(StringType()),
-    col("Embarazo").cast(BooleanType()),
-    col("Rel_emba").cast(BooleanType()),
-    col("Presunto").cast(BooleanType())
-)
-
-DEFUN_GRAL.write.mode("overwrite").parquet(os.path.join(parquet_folder, "DEFUN_GRAL.parquet"))
-DEFUN_ENAPEA.write.mode("overwrite").parquet(os.path.join(parquet_folder, "DEFUN_ENAPEA.parquet"))
+DEFUN = spark.read.option("mergeSchema", "true").parquet(*parquet_files)
+EDR = DEFUN.filter((DEFUN["Embarazo"].isin(1, 2, 3, 4)))
+EDR = EDR.select(
+    "Anio_ocur", "Ent_resid", "Mun_resid", "Loc_resid", "Edad", "Causa_def", "Edo_civil", 
+    "Escolarida", "Sitio_ocur", "Derechohab", "Embarazo", "Rel_emba", "Presunto", "Capitulo","Edad_agru")
+EDR.write.mode("overwrite").parquet(os.path.join(parquet_folder, "EDR_INEGI.parquet"))
 print("✅ Procesamiento completado. Archivos guardados en Parquet.")
+
+#------------------------------------------------------------------Alimentar la DB en MYSQL
+
+        
